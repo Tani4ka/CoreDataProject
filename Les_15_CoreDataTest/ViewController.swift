eb21e85962ff75a3672d0e8df29a2fe601d1070d
Tetiana <Tatyana.granchenko@gmail.com>
2019-06-27 12:52:01 +0300

    Initial Commit

---
+++ Les_15_CoreDataTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Les_15_CoreDataTest
+//
+//  Created by Tetiana Hranchenko on 6/27/19.
+//  Copyright © 2019 Tanya Corporation. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
